11f259e21f1c23bc59ee5894d8196c494cc3deae
votre_login <votre_mail>
2022-12-26 17:46:50 +0100

    .

--- main1.c
+++ main1.c
@@ -7,24 +7,45 @@ int main() {
     
     //initialisation du Labyrinthe
   int sizeX, sizeY, lab, tileN, tileE, tileS, tileW, tileItem;	
-  char *monLab[4096];
-  connectToServer("172.105.76.204", 1234 , "SarahLpb"); 
-  waitForLabyrinth("TRAINING DONTMOVE", monLab, &sizeX, &sizeY);
+  char *monLab[715];
+  connectToServer("172.105.76.204", 1234 , "Sarah"); 
+  waitForLabyrinth("TRAINING DONTMOVE start=0 timeout=1000 ", monLab, &sizeX, &sizeY);
   int turn = getLabyrinth(&lab, &tileN, &tileE, &tileS, &tileW, &tileItem);
   
   //mon tour de jouer 
-  
-  t_return_code WoL;
+  t_return_code WoL = NORMAL_MOVE;
   t_move *moveME;
+  moveME = malloc(sizeof(t_move));
   int number1,rotation1,X1,Y1;
   int insertion; // entre 0 et 3 
   t_insertion tuile;
+  int comments = 4;
+  char commentaires[500];
   
-		
+  //tour de l'adversaire
+  t_move *moveYOU; //pourquoi ça ne marche pas si c'est un pointeur 
+  moveYOU = malloc(sizeof(t_move)); 
+  
+	while(WoL == NORMAL_MOVE){
+		if(turn == 0 ){
 	 	printLabyrinth();
-	  	printf("%d, %d, %d, %d, %d, %d, %d, %d \n", sizeX, sizeY, lab, tileN, tileE, tileS, tileW, tileItem);
-	  		printf("insertion merci\n");
-	  		scanf("%d ", &insertion);
+	 	
+	 		//informations sur la partie en cours 
+		 	if(tileItem > 0){
+		 		printf("la tuile présente le trésor numéro : %d \n", tileItem);
+		 	}
+		 	else{
+		 		printf("il n'y a pas de trésor sur la prochaine tuile \n");
+		 	}
+		 	if( tileN>0 ) printf("il y a un mur direction Nord\n");
+		 	if( tileE>0 ) printf("il y a un mur direction Est\n");
+		 	if( tileS>0 ) printf("il y a un mur direction Sud\n");
+		 	if( tileW>0 ) printf("il y a un mur direction Ouest\n");
+	  		printf("valeur de lab %d \n", lab);
+	  		
+	  		//Paramètrage de getMove();
+	  		printf("Insérer la tuile \n 0 -> ligne gauche \n 1-> ligne droite \n 2-> colonne haut \n 3->colonne bas  \n ");
+	  		scanf("%d",&insertion );
 	  		switch(insertion){
 	  		case 0 : tuile = INSERT_LINE_LEFT; 
 	  			  break;
@@ -35,35 +56,78 @@ int main() {
 	  		case 3 : tuile = INSERT_COLUMN_BOTTOM;
 	  			break;
 	  		}
-	  		printf("number impaire qui indique le numéro de la ligne ou de la colonne où on insère la tuile\n");
-	  		scanf("%d",&number1);
-	  		printf("rotation qui indique de combien de quart de tour (entre 0 et 3)\n");
-	  		scanf("%d",&rotation1);
-	  		printf("x et y qui indique les coordonnées d’où se déplace le pion\n");
-	  		scanf("%d %d", &X1, &Y1);
+	  		printf("rentrez un nombre impaire qui indique le numéro de la ligne ou de la colonne où on insère la tuile\n");
+	  		scanf("%d", &number1 );
+	  		printf("indiquer le nombre de rotation de quart de tour pour insérer la tuile  (entre 0 et 3)\n");
+	  		scanf("%d", &rotation1 );
+	  		printf("Rentrez la ligne ou vous voulez déplacer votre pion (nombre entre 1 et %d) \n", sizeX);
+	  		scanf("%d", &X1);
+	  		printf("Rentrez la colonne ou vous voulez déplacer votre pion(nombre entre 1 et %d) \n", sizeY);
+	  		scanf("%d", &Y1);
 	  		moveME->insert = tuile;
 	  		moveME->number = number1;
 	  		moveME->rotation = rotation1;
+	  		moveME->x = X1;
+	  		moveME->y = Y1;
 	  		WoL = sendMove(moveME);
-	  		printf("ici");
+	  		printf("le prochain trésor à trouver est le numéro %d\n", moveME->nextItem);
+
+			//Commentaires
+			printf("Voulez vous envoyer un commentaire \n 0 pour oui et 1 pour non \n");
+			scanf("%d", &comments);
+			if(comments == 0){
+				printf("Rentrez le commentaire à envoyer\n");
+				scanf("%s", commentaires);
+				sendComment(commentaires);
+			}
+	  		
+	  		//condition de sortie
 	  		if(WoL == NORMAL_MOVE)
 	  		{
-	  			int suc = 1;
+	  			turn = (!turn) ;
+
 	  		}
 	  		if(WoL == WINNING_MOVE)
 	  		{
-	  			printf("Vous avez gagné");
+	  			printf("Vous avez gagné\n");
+	  			closeConnection();
 	 
 	  		}
 	  		if(WoL == LOOSING_MOVE)
 	  		{
-	  			printf("Vous avez perdu");
+	  			printf("Vous avez perdu\n");
+	  			closeConnection();
 	  		}
 	  		
+	  	}
 	  	
- 
+	  	//tour de l'adversaire
+	  	if( turn == 1){
+	  		printLabyrinth();
+	  		WoL = getMove(moveYOU);
+	  		printf("Notation : 0 -> ligne gauche \n 1-> ligne droite \n 2-> colonne haut \n 3->colonne bas \n");
+	  		printf("l'adversaire a cet endroit %d  \n", moveYOU->insert );
+	  		printf("la tuile de la ligne/colonne: %d a été déplacé \n", moveYOU->number );
+	  		printf("la tuile a été placé dans le sens %d \n", moveYOU->rotation);
+	  		printf("les coordonnées du précedent déplacement sont : %d et %d \n", moveYOU->x, moveYOU->y);	 	
+	  		if(WoL == NORMAL_MOVE)
+	  		{
+	  			int suc = 1;
+	  			turn = (!turn);
+	  		}
+	  		if(WoL == WINNING_MOVE)
+	  		{
+	  			printf("Vous avez gagné");
+	  			  closeConnection();
+	  		}
+	  		if(WoL == LOOSING_MOVE)
+	  		{
+	  			printf("Vous avez perdu");
+	  			  closeConnection();
+	  		}
+	  	}
 
-  
-  closeConnection();
-	
+
+  	}
+	closeConnection;
 }
